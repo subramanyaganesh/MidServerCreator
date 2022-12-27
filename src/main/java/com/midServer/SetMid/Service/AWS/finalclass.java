67d271970571825f5e64886f831d9465cc8c182b
Subramanya Ganesh <subramanyaganesh@dhcp55.merunorth101.iit.edu>
2022-12-27 14:35:08 -0600

    can be made public

--- src/main/java/com/midServer/SetMid/Service/AWS/finalclass.java
+++ src/main/java/com/midServer/SetMid/Service/AWS/finalclass.java
@@ -19,7 +19,7 @@ import static com.midServer.SetMid.Service.InstanceService.setValuesToCommand;
 public class finalclass {
     public static void main(String[] args) throws Exception {
         String AWS_ACCESS_KEY = "";
-        String AWS_SECRET_KEY = "IH3ctRqdQyffMZXnbom/xAS+6mxZ9t/uoGrNb5nk";
+        String AWS_SECRET_KEY = "";
         String regionName = "us-west-2";
         String keyName = "keyName";
         String instanceName = "discoinstance";
